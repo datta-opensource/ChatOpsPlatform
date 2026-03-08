bf323b6db7b828aa6fd044423dcb74d61607dc27
datta-opensource <datta.opensource@gmail.com>
2026-03-09 00:04:57 +0530

    Switch to SQLite and remove secrets from config

--- Program.cs
+++ Program.cs
@@ -1,12 +1,10 @@
 using Microsoft.EntityFrameworkCore;
 using SwiftLux.WhatsApp.Api.Configurations;
-using SwiftLux.WhatsApp.Api.Data;
 using SwiftLux.WhatsApp.Api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddScoped<IConversationService, ConversationService>();
 // Add services
 builder.Services.Configure<WhatsAppOptions>(
